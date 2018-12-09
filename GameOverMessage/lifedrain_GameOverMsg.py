60b83c4b2f3df0f7687bb20d78134c615b4d2fcc
Lovac42 <lovac42@protonmail.com>
2018-12-09 01:28:07 -0500

    wait for grading before GO screen

--- GameOverMessage/lifedrain_GameOverMsg.py
+++ GameOverMessage/lifedrain_GameOverMsg.py
@@ -2,13 +2,21 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/LifeDrain_EndGames
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.2b
+# Version: 0.0.3
 
+
+# == User Config =========================================
+
+#Don't show gameover screen util after the user grades the card.
+WAIT_FOR_GRADE = True
+
+# == End Config ==========================================
+##########################################################
 
 from aqt import mw
-from anki.hooks import addHook, runHook
+from anki.hooks import addHook, runHook, remHook
 from anki import version
-ANKI21 = version.startswith("2.1.")
+ANKI21=version.startswith("2.1.")
 
 
 ASCII_ART="""<center><h1>GAME OVER</h1><br><br><pre>
@@ -30,11 +38,35 @@ ASCII_ART="""<center><h1>GAME OVER</h1><br><br><pre>
 </pre></center>"""
 
 
-def msg():
+gameover=False
+def checkState():
+    global gameover
+    if WAIT_FOR_GRADE and \
+    mw.reviewer.state=='answer':
+        gameover=True
+        addHook('showQuestion',showMsg)
+    else:
+        showMsg()
+
+
+def showMsg():
     mw.requireReset(True)
     mw.bottomWeb.hide()
     mw.web.stdHtml(ASCII_ART,
         css='' if ANKI21 else mw.sharedCSS)
+    reset()
+
+
+def reset():
+    global gameover
+    gameover=False
+    remHook('showQuestion',showMsg)
     runHook('LifeDrain.recover',True,9999)
 
-addHook('LifeDrain.gameOver',msg)
+
+def onAfterStateChange(newS,oldS,*args):
+    if gameover: reset()
+
+
+addHook('LifeDrain.gameOver',checkState)
+addHook('afterStateChange',onAfterStateChange)
